17803142c5058243aecec16e28293d76f0348b5e
Petr Popov <petyap666@gmail.com>
2022-04-20 01:06:56 +0300

    add test_del_project.py

---
+++ test/test_del_project.py
@@ -0,0 +1,13 @@
+from model.project import Project
+from random import randrange
+
+
+def test_del_some_project(app):
+    app.project.create_project_if_it_not_exist(Project(name="test"))
+    old_projects = app.project.get_project_list()
+    index = randrange(len(old_projects))
+    app.project.delete_project_by_index(index)
+    new_projects = app.project.get_project_list()
+    assert len(old_projects) - 1 == len(new_projects)
+    del old_projects[index]
+    assert sorted(old_projects, key=Project.id_or_max) == sorted(new_projects, key=Project.id_or_max)
