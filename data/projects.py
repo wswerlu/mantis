d53a81c3aece46d0e9b091e4e12613a87010bcdf
Petr Popov <petyap666@gmail.com>
2022-04-20 01:02:46 +0300

    add data

---
+++ data/projects.py
@@ -0,0 +1,8 @@
+from model.project import Project
+
+
+testdata = [
+    Project(name="name1", description="description1"),
+    Project(name="name2", description="description3"),
+    Project(name="name3", description="description3")
+]
