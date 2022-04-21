8fa355940e7af9d19312bc6fbca55357549683d8
Petr Popov <petyap666@gmail.com>
2022-04-21 23:17:39 +0300

    update application.py

--- fixture/application.py
+++ fixture/application.py
@@ -1,10 +1,13 @@
 from selenium import webdriver
 from fixture.session import SessionHelper
 from fixture.project import ProjectHelper
+from fixture.james import JamesHelper
+from fixture.signup import SignupHelper
+from fixture.mail import MailHelper
 
 
 class Application:
-    def __init__(self, browser, base_url):
+    def __init__(self, browser, config):
         if browser == "firefox":
             self.wd = webdriver.Firefox()
         elif browser == "chrome":
@@ -15,7 +18,11 @@ class Application:
             raise ValueError(f"Unrecognized {browser}")
         self.session = SessionHelper(self)
         self.project = ProjectHelper(self)
-        self.base_url = base_url
+        self.james = JamesHelper(self)
+        self.signup = SignupHelper(self)
+        self.mail = MailHelper(self)
+        self.config = config
+        self.base_url = config["web"]["baseUrl"]
 
     def is_valid(self):
         try:
