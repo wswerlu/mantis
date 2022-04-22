78bfb83f0536d12eb55d7fe4683233e349c1f9f8
Petr Popov <petyap666@gmail.com>
2022-04-22 20:05:02 +0300

    refactor conftest.py

--- conftest.py
+++ conftest.py
@@ -31,7 +31,7 @@ def app(request, config):
     browser = request.config.getoption("--browser")
     if fixture is None or not fixture.is_valid():
         fixture = Application(browser=browser, config=config)
-    # fixture.session.ensure_login(username=config["webadmin"]["username"], password=config["webadmin"]["password"])
+    fixture.session.ensure_login(username=config["webadmin"]["username"], password=config["webadmin"]["password"])
     return fixture
 
 
