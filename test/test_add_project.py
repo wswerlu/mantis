332fecf0c06d259284646d384d86bd8bf76fd12c
Petr Popov <petyap666@gmail.com>
2022-04-22 21:26:59 +0300

    refactor test_add_project.py

--- test/test_add_project.py
+++ test/test_add_project.py
@@ -1,14 +1,16 @@
 from model.project import Project
 
 
-def test_add_project(app, data_projects):
+def test_add_project(app, data_projects, config):
     project = data_projects
-    old_projects = app.project.get_project_list()
+    old_projects = app.soap.get_project_list(
+        username=config["webadmin"]["username"], password=config["webadmin"]["password"])
     if project in old_projects:
         app.project.delete_project_by_name(project.name)
         old_projects.remove(project)
     app.project.create(project)
-    new_projects = app.project.get_project_list()
+    new_projects = app.soap.get_project_list(
+        username=config["webadmin"]["username"], password=config["webadmin"]["password"])
     assert len(old_projects) + 1 == len(new_projects)
     old_projects.append(project)
     assert sorted(old_projects, key=Project.id_or_max) == sorted(new_projects, key=Project.id_or_max)
