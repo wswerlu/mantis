eb5708fdfecb3f2d3f0fed59c0a83218b9adb095
Petr Popov <petyap666@gmail.com>
2022-04-22 20:02:57 +0300

    refactor test_login.py

--- test/test_login.py
+++ test/test_login.py
@@ -1,3 +1,4 @@
-def test_login(app):
-    app.session.login("administrator", "root")
-    assert app.session.is_logged_in_as("administrator")
+def test_login(app, config):
+    app.session.ensure_logout()
+    app.session.login(username=config["webadmin"]["username"], password=config["webadmin"]["password"])
+    assert app.session.is_logged_in_as(config["webadmin"]["username"])
