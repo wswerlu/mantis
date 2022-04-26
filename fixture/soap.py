6579c60477bcc72892509a701e454628cb4e8754
Petr Popov <petyap666@gmail.com>
2022-04-26 20:49:40 +0300

    refactor fixture/soap.py

--- fixture/soap.py
+++ fixture/soap.py
@@ -8,17 +8,24 @@ class SoapHelper:
         self.app = app
 
     def can_login(self, username, password):
-        client = Client("http://localhost/mantisbt/api/soap/mantisconnect.php?wsdl")
+        client = self.get_client()
         try:
             client.service.mc_login(username, password)
             return True
         except WebFault:
             return False
 
-    def get_project_list(self, username, password):
-        client = Client("http://localhost/mantisbt/api/soap/mantisconnect.php?wsdl")
+    def get_client(self):
+        base_url = self.app.config['web']['baseUrl']
+        client = Client(base_url + "/api/soap/mantisconnect.php?wsdl")
+        return client
+
+    def get_project_list(self):
+        client = self.get_client()
         projects = []
-        projects_mc = client.service.mc_projects_get_user_accessible(username, password)
+        config = self.app.config['webadmin']
+        projects_mc = client.service.mc_projects_get_user_accessible(
+            username=config["username"], password=config["password"])
         for project in projects_mc:
             projects.append(Project(id=project.id, name=project.name))
         return projects
